2182892fd19706adf8a2c7b65614f7d955e14e8f
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-28 22:50:07 +0300

    user service update method added

--- backEnd/api/@modules/User/Service/user.service.ts
+++ backEnd/api/@modules/User/Service/user.service.ts
@@ -1,15 +1,41 @@
-import { UserDal } from '../Dal/user.dal'
+import { UserDal } from "../Dal/user.dal";
 
 export class UserService {
-    private userDataAccess:UserDal = new UserDal()
-    findAll() {
-        return {
-            user:this.userDataAccess.findAll()
-        }
+  private userDataAccess: UserDal = new UserDal();
+  findAll() {
+    return {
+      user: this.userDataAccess.findAll(),
+    };
+  }
+  find(id: string) {
+    return {
+      user: this.userDataAccess.find(id),
+    };
+  }
+  async update(
+    id: string,
+    name: string,
+    surname: string,
+    date: string,
+    gender: string,
+    biography: string
+  ) {
+    const user = await this.userDataAccess.update(
+      id,
+      name,
+      surname,
+      date,
+      gender,
+      biography
+    );
+    if (user.message) {
+      return {
+        user: user.message,
+      };
+    } else {
+      return {
+        user,
+      };
     }
-    find(id:string) {
-        return {
-            user:this.userDataAccess.find(id)
-        }
-    }
+  }
 }
