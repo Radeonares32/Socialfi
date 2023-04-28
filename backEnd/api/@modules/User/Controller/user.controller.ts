ad7544f0e394354a97c6e2909631a80562211171
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-28 23:13:55 +0300

    user controller postUser method added

--- backEnd/api/@modules/User/Controller/user.controller.ts
+++ backEnd/api/@modules/User/Controller/user.controller.ts
@@ -7,13 +7,26 @@ export class UserController {
 
   getUsers: Handler = async (_req, res) => {
     res.json({
-      user: await this.userService.findAll().user
+      user: await this.userService.findAll().user,
     });
   };
   getUser: Handler = async (req, res) => {
     const { id } = req.query as any;
     res.json({
-      user: this.userService.find(id).user
+      user: this.userService.find(id).user,
+    });
+  };
+  postUser: Handler = async (req, res) => {
+    const { id, name, surname, date, gender, biography } = req.body;
+    res.json({
+      user: await this.userService.create(
+        id,
+        name,
+        surname,
+        date,
+        gender,
+        biography
+      ),
     });
   };
 }
