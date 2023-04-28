53b2d47f716ac03cf729bffc22b2c1a5b7e61f80
Radeonares32 <bugra.linux.js@gmail.com>
2023-04-28 23:41:08 +0300

    server console log

--- backEnd/server.ts
+++ backEnd/server.ts
@@ -10,4 +10,6 @@ import { userRoutes } from "./api/@modules/User/Route/routes";
 
 app.use("/", userRoutes);
 
-server.listen(3000);
+server.listen(3000,()=>{
+    console.log("server running")
+});
