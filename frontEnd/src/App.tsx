98bd6da4fdc248f630096ac4c8071f38c012c0e3
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-01 04:07:12 +0300

    navbar add

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,16 +1,11 @@
-import './assets/css/bootstrap.min.css'
-import './assets/css/line-awesome.min.css'
-import './assets/css/all.min.css'
-import './assets/css/fontawesome.min.css'
-import './assets/css/slick.css'
-import './assets/css/slick-theme.css'
-import './assets/css/flatpickr.min.css'
-import './assets/css/style.css'
-import './assets/css/responsive.css'
+import { Home } from "./components/home/home";
 
 function App() {
-
-  return <></>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ frontEnd/src/components/home/home.tsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./navbar/navbar";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="main-wrapper">
+        <Navbar />
+      </div>
+    </>
+  );
+};
