763a06a5d996d45a102d957d57fc265393d575f2
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 21:48:08 +0300

    notification controller getNotification method created

--- backEnd/api/@modules/Notification/Controller/notification.controller.ts
+++ backEnd/api/@modules/Notification/Controller/notification.controller.ts
@@ -1,14 +1,23 @@
-import { Handler } from 'express'
+import { Handler } from "express";
 
 import { NotificaitonService } from "../Service/notification.service";
 
 export class NotificationController {
-    private notificationService:NotificaitonService = new NotificaitonService()
+  private notificationService: NotificaitonService = new NotificaitonService();
 
-    getNotifications:Handler = async (req,res) => {
-        const  token  = req.headers['x-access-token'] as string
-        res.json({
-            notification:(await (this.notificationService.findAll(token))).notification
-        })
-    }
+  getNotifications: Handler = async (req, res) => {
+    const token = req.headers["x-access-token"] as string;
+    res.json({
+      notification: (await this.notificationService.findAll(token))
+        .notification,
+    });
+  };
+  getNotification: Handler = async (req, res) => {
+    const token = req.headers["x-access-token"] as string;
+    const { id } = req.body;
+    res.json({
+      notification: (await this.notificationService.find(id, token))
+        .notification,
+    });
+  };
 }
