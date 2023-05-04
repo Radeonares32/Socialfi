4385197724f685c58e5ffad6889000f975505971
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 21:49:42 +0300

    notification find method added main route

--- backEnd/api/@modules/Notification/Route/routes.ts
+++ backEnd/api/@modules/Notification/Route/routes.ts
@@ -1,9 +1,10 @@
 import express, { Handler } from "express";
 const app = express.Router();
 
-import { getNotifications } from "./notification.route";
+import { getNotifications,getNotification } from "./notification.route";
 
 export const notificaitonRoutes: Handler = app.use(
   "/notification",
-  getNotifications
+  getNotifications,
+  getNotification
 );
