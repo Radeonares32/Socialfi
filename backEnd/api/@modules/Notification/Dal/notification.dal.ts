66bfb4e6133481dff0143a6873d9792615908c68
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 13:32:50 +0300

    INotification dal findAll method created

--- backEnd/api/@modules/Notification/Dal/notification.dal.ts
+++ backEnd/api/@modules/Notification/Dal/notification.dal.ts
@@ -1,9 +1,25 @@
 import { INotification } from "../Entity/INotification";
 import { NotificationRepository } from "../Repository/notification.repo";
 
+import { neo4j } from "../../../../core/dataSource/neo4j/neo4j";
+
 export class INotificationDal implements NotificationRepository {
   findAll(): Promise<INotification[]> {
-    throw new Error("Method not implemented.");
+    return new Promise(async (resolve, reject) => {
+      try {
+        const notifications: any = await neo4j()
+          ?.readCypher("match(n:notification) return n", {})
+          .catch((err) => console.log(err));
+        const rNot = notifications.records.map((uss: any) => {
+          return uss.map((res: any) => {
+            return res.properties;
+          });
+        });
+        resolve(rNot as INotification[]);
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
   }
   find(id: string): Promise<INotification> {
     throw new Error("Method not implemented.");
