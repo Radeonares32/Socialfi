9bde039c63818b1841b21f279fde0cbcaaf1a603
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 06:42:50 +0300

    react auth kit index.tsx injected

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "react-auth-kit";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider
+    authType={"cookie"}
+    authName={"_auth"}
+    cookieDomain={window.location.hostname}
+    cookieSecure={window.location.protocol === "https:"}
+  >
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
