fc07a6da506c960e91aca0fd1ec26954c7b78463
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 21:40:55 +0300

    notification route userAuth middleware injected

--- backEnd/api/@modules/Notification/Route/notification.route.ts
+++ backEnd/api/@modules/Notification/Route/notification.route.ts
@@ -2,10 +2,12 @@ import express,{Handler} from 'express'
 
 import {NotificationController } from '../Controller/notification.controller'
 
+import { Middlewares } from '../../../middleware/middleware'
+
 const app = express.Router()
 
 const notificaitonController:NotificationController = new NotificationController()
 
 
 //Get
-export const getNotifications:Handler = app.get('/',notificaitonController.getNotifications)
+export const getNotifications:Handler = app.get('/',Middlewares.userAuth,notificaitonController.getNotifications)
