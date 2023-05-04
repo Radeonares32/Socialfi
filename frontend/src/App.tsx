e4d38163b8f4f30d47923a4b5395ebfafd6df8c4
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-04 11:34:02 +0300

    first private route added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import { Home } from "./components/home/Home";
 import { Register } from "./components/register/register";
+import { Information } from "./components/account/information/information";
 
 import { Routes, Route } from "react-router-dom";
+import { RequireAuth } from "react-auth-kit";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="register" element={<Register />} />
+      <Route
+        path="information"
+        element={
+          <RequireAuth loginPath="/">
+            <Information />
+          </RequireAuth>
+        }
+      />
     </Routes>
   );
 }
