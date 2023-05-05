8de5644c9bbf73b4ba62cc61d8cb79a3579f8dd7
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-05 20:41:23 +0300

    post dal findAll method created

---
+++ backEnd/api/@modules/Post/Dal/post.dal.ts
@@ -0,0 +1,57 @@
+import { security } from "../../../security/security";
+import { neo4j } from "../../../../core/dataSource/neo4j/neo4j";
+
+import { PostRepository } from "../Repository/post.repository";
+import { IPost } from "../Entity/IPost";
+
+export class PostDal implements PostRepository {
+  findAll(): Promise<IPost[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const post: any = await neo4j()
+          ?.readCypher("match(p:post) return p", {})
+          .catch((err) => console.log(err));
+        const rPost = post.records.map((uss: any) => {
+          return uss.map((res: any) => {
+            return res.properties;
+          });
+        });
+        resolve(rPost as IPost[]);
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findAllUser(walletAddr: string): Promise<IPost[]> {
+    throw new Error("Method not implemented.");
+  }
+  find(id: string): Promise<IPost> {
+    throw new Error("Method not implemented.");
+  }
+  findUser(id: string, walletAddr: string): Promise<IPost> {
+    throw new Error("Method not implemented.");
+  }
+  create(
+    id: string,
+    walletAddr: string,
+    title: string,
+    description: string,
+    date: string,
+    image?: string | undefined
+  ): Promise<{ message: string }> {
+    throw new Error("Method not implemented.");
+  }
+  update(
+    id: string,
+    walletAddr: string,
+    title: string,
+    description: string,
+    date: string,
+    image?: string | undefined
+  ): Promise<{ message: string }> {
+    throw new Error("Method not implemented.");
+  }
+  delete(id: string, walletAddr: string): Promise<{ message: string }> {
+    throw new Error("Method not implemented.");
+  }
+}
