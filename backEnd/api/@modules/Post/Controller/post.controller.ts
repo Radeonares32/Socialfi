8f2c90826a61abb9301c45d4b6603cd31fed5e31
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-05 22:26:38 +0300

    post contr getPostUserFindAll method created

--- backEnd/api/@modules/Post/Controller/post.controller.ts
+++ backEnd/api/@modules/Post/Controller/post.controller.ts
@@ -1,12 +1,18 @@
-import { Handler } from 'express'
+import { Handler } from "express";
 
-import { PostService } from '../Service/post.service'
+import { PostService } from "../Service/post.service";
 
 export class PostController {
-    private postService:PostService = new PostService()
-    getPostFindAll:Handler = async (_req,res) => {
-        res.json({
-            post:(await this.postService.findAll()).post
-        })
-    }
+  private postService: PostService = new PostService();
+  getPostFindAll: Handler = async (_req, res) => {
+    res.json({
+      post: (await this.postService.findAll()).post,
+    });
+  };
+  getPostUserFindAll: Handler = async (req, res) => {
+    const token = req.headers["x-access-token"] as string;
+    res.json({
+      post: (await this.postService.findAllUser(token)).post,
+    });
+  };
 }
