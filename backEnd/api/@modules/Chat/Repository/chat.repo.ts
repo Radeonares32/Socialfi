4045c7a7d2f073db531f147d363eace2d5006dca
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 08:49:21 +0300

    chat repo created

---
+++ backEnd/api/@modules/Chat/Repository/chat.repo.ts
@@ -0,0 +1,23 @@
+import { IChatMessage } from "../Entity/IChatMessage";
+import { IChatRoom } from "../Entity/IChatRoom";
+import { IUser } from "../../User/Entity/IUser";
+
+export interface ChatRepository {
+  findAllChat(): Promise<IChatRoom[]>;
+  findChat(chatId: string): Promise<IChatRoom>;
+  findAllMessage(): Promise<IChatMessage[]>;
+  findMessage(messageId: string): Promise<IChatMessage>;
+  findUserChatRoom(chatId: string): Promise<IUser>;
+  findChatRoomUser(walletAddr: string): Promise<IChatRoom>;
+  findChatMessages(chatId: string): Promise<IChatMessage[]>;
+  findChatMessageUser(
+    chatId: string,
+    walletAddr: string
+  ): Promise<IChatMessage[]>;
+  createChatRoom(): Promise<{ message: string }>;
+  createUserMessage(
+    chatId: string,
+    walletAddr: string,
+    message: string
+  ): Promise<{ message: string }>;
+}
