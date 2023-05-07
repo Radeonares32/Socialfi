159611f9860aadbee3a8ded36edb9710d985b9eb
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 10:43:03 +0300

    chat serv getFindMessage method created

--- backEnd/api/@modules/Chat/Service/chat.service.ts
+++ backEnd/api/@modules/Chat/Service/chat.service.ts
@@ -1,28 +1,38 @@
-import { ChatDal } from '../Dal/chat.dal'
+import { ChatDal } from "../Dal/chat.dal";
 
 export class ChatService {
-    private chatDal:ChatDal = new ChatDal()
+  private chatDal: ChatDal = new ChatDal();
 
-    async getFindAllChat() {
-        return {
-            chat:await this.chatDal.findAllChat()
-        }
+  async getFindAllChat() {
+    return {
+      chat: await this.chatDal.findAllChat(),
+    };
+  }
+  async getFindChat(chatId: string) {
+    if (chatId) {
+      return {
+        chat: await this.chatDal.findChat(chatId),
+      };
+    } else {
+      return {
+        chat: "chatId not found",
+      };
     }
-    async getFindChat(chatId:string) {
-        if(chatId) {
-            return {
-                chat:await this.chatDal.findChat(chatId)
-            }
-        }
-        else {
-            return {
-                chat:"chatId not found"
-            }
-        }
+  }
+  async getFindAllMessage() {
+    return {
+      chat: await this.chatDal.findAllMessage(),
+    };
+  }
+  async getFindMessage(messageId: string) {
+    if (messageId) {
+      return {
+        chat: await this.chatDal.findMessage(messageId),
+      };
+    } else {
+      return {
+        chat: "chatId not found",
+      };
     }
-    async findAllMessage() {
-        return {
-            chat:await this.chatDal.findAllMessage()
-        }
-    }
+  }
 }
