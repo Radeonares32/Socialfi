1d4583c0368d62767d0e0d9a7083d4b9de802487
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 11:23:22 +0300

    chat routes  method created

---
+++ backEnd/api/@modules/Chat/Route/chat.route.ts
@@ -0,0 +1,25 @@
+import express,{Handler} from 'express'
+
+import { ChatController } from '../Controller/chat.controller'
+import { Middlewares } from '../../../middleware/middleware'
+
+const app = express.Router()
+const chatController:ChatController = new ChatController()
+
+
+//Get
+export const  getFindAllChatRoute:Handler = app.get('/findAllChat',Middlewares.userAuth,chatController.getFindAllChat)
+export const getFindChatRoute:Handler = app.get('/findChat',Middlewares.userAuth,chatController.getFindChat)
+export const getFindAllMessageRoute:Handler = app.get('/findAllMessage',Middlewares.userAuth,chatController.getFindAllMessage)
+export const getFindMessageRoute:Handler = app.get('/findMessage',Middlewares.userAuth,chatController.getFindMessage)
+export const getFindUserChatRoomRoute:Handler = app.get('/findUserChatRoom',Middlewares.userAuth,chatController.getFindUserChatRoom)
+export const getFindChatRoomUserRoute:Handler = app.get('/findChatRoomUser',Middlewares.userAuth,chatController.getFindChatRoomUser)
+export const getFindChatMessagesRoute:Handler = app.get('/findChatMessages',Middlewares.userAuth,chatController.getFindChatMessages)
+export const getFindChatMessageUserRoute:Handler = app.get('/findChatMessageUser',Middlewares.userAuth,chatController.getFindChatMessageUser)
+export const getFindUserMessageRoute:Handler = app.get('/findUserMessage',Middlewares.userAuth,chatController.getFindUserMessage)
+export const getFindMessageUserRoute:Handler = app.get('/findMessageUser',Middlewares.userAuth,chatController.getFindMessageUser)
+
+
+//Post
+export const postCreateChatRoomRoute:Handler = app.get('/createChatRoom',Middlewares.userAuth,chatController.postCreateChatRoom)
+export const postCreateUserMessageRoute:Handler = app.get('/createUserMessage',Middlewares.userAuth,chatController.postCreateUserMessage)
