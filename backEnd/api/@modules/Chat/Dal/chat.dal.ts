9948aaa9f4b7545a67ef38a864d82a2fa1585b2d
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 09:24:22 +0300

    chat dal findAll & find method created

---
+++ backEnd/api/@modules/Chat/Dal/chat.dal.ts
@@ -0,0 +1,115 @@
+import { IChatMessage } from "../Entity/IChatMessage";
+import { IChatRoom } from "../Entity/IChatRoom";
+import { IUser } from "../../User/Entity/IUser";
+
+import { ChatRepository } from "../Repository/chat.repo";
+import { neo4j } from "../../../../core/dataSource/neo4j/neo4j";
+
+export class ChatDal implements ChatRepository {
+  findAllChat(): Promise<IChatRoom[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const chat: any = await neo4j()
+          ?.readCypher("match (c:chat) return c", {})
+          .catch((err) => console.log(err));
+        const rChat: any = chat.records.map((uss: any) => {
+          return uss.map((res: any) => {
+            return res.properties;
+          });
+        });
+        resolve(rChat as IChatRoom[]);
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findChat(chatId: string): Promise<IChatRoom> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const chat: any = await neo4j()?.readCypher(
+          "match (c:chat {id:$chatId}) return c",
+          { chatId }
+        );
+        const rChat: any = chat.records.map((uss: any) => {
+          return uss.map((res: any) => {
+            return res.properties;
+          });
+        });
+        resolve(rChat as IChatRoom);
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findAllMessage(): Promise<IChatMessage[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findMessage(messageId: string): Promise<IChatMessage> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findUserChatRoom(chatId: string): Promise<IUser> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findChatRoomUser(walletAddr: string): Promise<IChatRoom> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findChatMessages(chatId: string): Promise<IChatMessage[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  findChatMessageUser(
+    chatId: string,
+    walletAddr: string
+  ): Promise<IChatMessage[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  createChatRoom(): Promise<{ message: string }> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+  createUserMessage(
+    chatId: string,
+    walletAddr: string,
+    message: string
+  ): Promise<{ message: string }> {
+    return new Promise(async (resolve, reject) => {
+      try {
+      } catch (err) {
+        reject({ message: err });
+      }
+    });
+  }
+}
