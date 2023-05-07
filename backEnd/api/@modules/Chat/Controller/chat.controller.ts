f743db7634ebaf702108a9f432eec28955732c50
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 11:08:24 +0300

    chat cont getFindChatRoomUser method created

--- backEnd/api/@modules/Chat/Controller/chat.controller.ts
+++ backEnd/api/@modules/Chat/Controller/chat.controller.ts
@@ -1,35 +1,41 @@
-import { Handler } from 'express'
-import { ChatService } from '../Service/chat.service'
+import { Handler } from "express";
+import { ChatService } from "../Service/chat.service";
 
 export class ChatController {
-    private chatService:ChatService = new ChatService()
+  private chatService: ChatService = new ChatService();
 
-    getFindAllChat:Handler = async (_req,res) => {
-        res.json({
-            chat:(await this.chatService.getFindAllChat()).chat
-        })
-    }
-    getFindChat:Handler = async (req,res) => {
-        const { chatId } = req.body
-        res.json({
-            chat:(await this.chatService.getFindChat(chatId)).chat
-        })
-    }
-    getFindAllMessage:Handler = async (_req,res) => {
-        res.json({
-            chat:(await this.chatService.getFindAllMessage()).chat
-        })
-    }
-    getFindMessage:Handler = async (req,res) => {
-        const { messageId } = req.body
-        res.json({
-            chat:(await this.chatService.getFindMessage(messageId)).chat
-        })
-    }
-    getFindUserChatRoom:Handler = async (req,res) => {
-        const { chatId } = req.body
-        res.json({
-            chat:(await this.chatService.getFindUserChatRoom(chatId)).chat
-        })
-    }
+  getFindAllChat: Handler = async (_req, res) => {
+    res.json({
+      chat: (await this.chatService.getFindAllChat()).chat,
+    });
+  };
+  getFindChat: Handler = async (req, res) => {
+    const { chatId } = req.body;
+    res.json({
+      chat: (await this.chatService.getFindChat(chatId)).chat,
+    });
+  };
+  getFindAllMessage: Handler = async (_req, res) => {
+    res.json({
+      chat: (await this.chatService.getFindAllMessage()).chat,
+    });
+  };
+  getFindMessage: Handler = async (req, res) => {
+    const { messageId } = req.body;
+    res.json({
+      chat: (await this.chatService.getFindMessage(messageId)).chat,
+    });
+  };
+  getFindUserChatRoom: Handler = async (req, res) => {
+    const { chatId } = req.body;
+    res.json({
+      chat: (await this.chatService.getFindUserChatRoom(chatId)).chat,
+    });
+  };
+  getFindChatRoomUser: Handler = async (req, res) => {
+    const token = req.headers["x-access-token"] as string;
+    res.json({
+      chat: (await this.chatService.getFindChatRoomUser(token)).chat,
+    });
+  };
 }
