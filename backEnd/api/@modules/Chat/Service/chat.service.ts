5be8edd113e308d266279bdcefe3fd6f39514f70
Radeonares32 <bugra.linux.js@gmail.com>
2023-05-07 10:41:01 +0300

    chat serv getFindChat method created

--- backEnd/api/@modules/Chat/Service/chat.service.ts
+++ backEnd/api/@modules/Chat/Service/chat.service.ts
@@ -8,4 +8,16 @@ export class ChatService {
             chat:await this.chatDal.findAllChat()
         }
     }
+    async getFindChat(chatId:string) {
+        if(chatId) {
+            return {
+                chat:await this.chatDal.findChat(chatId)
+            }
+        }
+        else {
+            return {
+                chat:"chatId not found"
+            }
+        }
+    }
 }
